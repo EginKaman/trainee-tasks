ee3e404c304bebed5711c3bed011182e4ee281b7
Ivan Tiutiunnyk <ivan.tiutiunnyk@abz.agency>
2023-01-30 16:37:14 +0200

    fix: remove replyTo

--- app/Mail/Feedback.php
+++ app/Mail/Feedback.php
@@ -40,7 +40,6 @@ class Feedback extends Mailable implements ShouldQueue
         return new Envelope(
             from: new Address(config('mail.from.address'), config('mail.from.name')),
             to: new Address($this->message->email, $this->message->email),
-            replyTo: new Address($this->message->email, $this->message->email),
             subject: 'Feedback'
         );
     }
