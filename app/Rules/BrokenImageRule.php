632fbca1d9e8ea728793f9156ed277b82bb200f9
Ivan Tiutiunnyk <ivan.tiutiunnyk@abz.agency>
2023-02-16 10:47:39 +0200

    refactor (images): move image to di.

--- app/Rules/BrokenImageRule.php
+++ app/Rules/BrokenImageRule.php
@@ -9,10 +9,15 @@ use Illuminate\Contracts\Validation\Rule;
 
 class BrokenImageRule implements Rule
 {
+    public function __construct(
+        private readonly Image $image
+    ) {
+    }
+
     public function passes($attribute, $value): bool
     {
         try {
-            $image = app(Image::class)->readImage($value->getRealPath());
+            $image = $this->image->readImage($value->getRealPath());
         } catch (\ImagickException $exception) {
             return false;
         }
