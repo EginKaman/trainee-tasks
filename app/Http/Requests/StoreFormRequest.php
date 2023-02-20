1367da51597a62dba7b9697f91eb9129838d2ad3
Ivan Tiutiunnyk <ivan.tiutiunnyk@abz.agency>
2023-02-20 10:41:31 +0200

    fix (form): error validation with long string.

--- app/Http/Requests/StoreFormRequest.php
+++ app/Http/Requests/StoreFormRequest.php
@@ -11,23 +11,26 @@ class StoreFormRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required', 'min:2', 'max:128', 'regex:/^[A-Za-z ]{2,128}$/'],
-            'email' => ['required', 'min:6', 'max:254', 'regex:/^\\S+@\\S+\\.\\S+$/'],
+            'name' => ['required', 'string', 'min:2', 'max:128', 'regex:/^[A-Za-z ]{2,128}$/'],
+            'email' => ['required', 'string', 'min:6', 'max:254', 'regex:/^\\S+@\\S+\\.\\S+$/'],
             'email_rfc' => [
                 'nullable',
+                'string',
                 "regex:/^(?:[a-z0-9!#$%&'*+\\/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+\\/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9]{2,}(?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])$/",
             ],
-            'id' => ['nullable', 'min:2', 'max:128', 'regex:/^[a-z0-9][a-z0-9_]{2,128}[a-z0-9]$/'],
+            'id' => ['nullable', 'string', 'min:2', 'max:128', 'regex:/^[a-z0-9][a-z0-9_]{2,128}[a-z0-9]$/'],
             'phone' => [
                 'required',
+                'string',
                 'regex:/^\+38?[-.\s]?\(?0\d{1,2}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/',
             ],
             'additional_phone' => [
                 'nullable',
+                'string',
                 'regex:/\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\\s]?\d{1,4}[-.\s]?\d{1,9}[ ,;]?/',
             ],
-            'pincode' => ['required', 'regex:/^\d{4}-?\d{4}$/'],
-            'description' => ['nullable', 'max:500', 'regex:/.{0,500}/'],
+            'pincode' => ['required', 'string', 'regex:/^\d{4}-?\d{4}$/'],
+            'description' => ['nullable', 'string', 'max:500', 'regex:/.{0,500}/'],
         ];
     }
 
