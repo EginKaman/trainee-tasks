87618050c22eec7987d7dd8cd05bb0fec97e7876
Ivan Tiutiunnyk <ivan.tiutiunnyk@abz.agency>
2023-02-22 12:28:56 +0200

    fix (form): phone validation in some situations.

--- app/Http/Requests/StoreFormRequest.php
+++ app/Http/Requests/StoreFormRequest.php
@@ -6,6 +6,16 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
+/**
+ * @property string $name
+ * @property string $email
+ * @property string $email_rfc
+ * @property string $id
+ * @property string $phone
+ * @property string $additional_phone
+ * @property string $pincode
+ * @property string $description
+ */
 class StoreFormRequest extends FormRequest
 {
     public function rules(): array
@@ -17,14 +27,14 @@ class StoreFormRequest extends FormRequest
                 'string',
                 'min:6',
                 'max:254',
-                "regex:/^[a-z][a-z0-9!#$%&\\'*+\\/=?^_`{|}~-]+(?:\\\\.[a-z0-9!#$%&\\'*+\\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i",
+                "regex:/^[a-z][a-z0-9!#$%&\\'*+\\/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\\'*+\\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i",
             ],
             'email_rfc' => [
                 'nullable',
                 'string',
                 'min:6',
                 'max:254',
-                'regex:/^(?:[\sa-z0-9!#$%&\'*+\.\/\\\\=?^_`{|}~-]+(?:\.[\sa-z0-9!#$%&\'\"*+\.\/\\\\=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\ \x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z]){1,}?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/i',
+                'regex:/^(?:[\sa-z0-9!#$%&\'*+\.\/\\\\=?^_`{|}~-]+(?:\.[\sa-z0-9!#$%&\'\"*+\.\/\\\\=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\ \x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z]){1,}?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/i',
             ],
             'id' => ['nullable', 'string', 'min:2', 'max:128', 'regex:/^[a-z0-9](?!.*__.*)([a-z0-9_)]+)?[a-z0-9]$/'],
             'phone' => [
@@ -38,7 +48,8 @@ class StoreFormRequest extends FormRequest
                 'string',
                 'min:7',
                 'max:256',
-                'regex:/\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\\s]?\d{1,4}[-.\s]?\d{1,9}[ ,;]?/',
+                'regex:/.*\d{6,}.*/',
+                'regex:/^((\+?(\d{1,4}?[\-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{2,9})(?:[,;]? ?))+)[^\D\s]$/',
             ],
             'pincode' => ['required', 'string', 'min:8', 'max:9', 'regex:/^\d{4}-?\d{4}$/'],
             'description' => ['nullable', 'string', 'max:500', 'regex:/.{0,500}/'],
@@ -53,5 +64,16 @@ class StoreFormRequest extends FormRequest
             'name.regex' => 'Only Latin and spaces are used for the name',
             'phone.regex' => 'The phone format +38 (xxx) xxx-xx-xx',
         ];
+    }
+
+    protected function prepareForValidation(): void
+    {
+        $this->merge([
+            'pincode' => preg_replace('/ /', '', $this->pincode),
+            'id' => preg_replace('/ /', '', $this->id),
+            'email' => preg_replace('/ /', '', $this->email),
+            'email_rfc' => preg_replace('/ /', '', $this->email_rfc),
+            'phone' => preg_replace('/ /', '', $this->phone),
+        ]);
     }
 }
