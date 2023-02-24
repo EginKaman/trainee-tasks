effcd4a042ec2e87757e5938c80d36d83d888d1e
Ivan Tiutiunnyk <ivan.tiutiunnyk@abz.agency>
2023-02-24 16:13:01 +0200

    fix (form): phone validation with some symbols and situations

--- app/Http/Requests/StoreFormRequest.php
+++ app/Http/Requests/StoreFormRequest.php
@@ -48,7 +48,7 @@ class StoreFormRequest extends FormRequest
                 'string',
                 'min:7',
                 'max:256',
-                'regex:/^((\\+?(\\d{1,4}?([\\-.\\s]+)?\\(?\\d{1,3}?\\)?([-.\\s]+)?\\d{2,4}([-.\\s]+)?\\d{1,4}([-.\\s]+)?\\d{1,9})(?:[,;]?([\\s]+)?))+)(?<=\\d)$/m',
+                'regex:/^((\+?(\d{1,4}?([\-.\s]+)?\(?\d{1,3}?\)?([-.\s]+)?\d{2,4}([-.\s]+)?\d{1,4}([-.\s]+)?\d{2,9})(?:[,;]?([\s]+)?))+)(?<=\d)$/i',
             ],
             'pincode' => ['required', 'string', 'min:8', 'max:9', 'regex:/^\d{4}-?\d{4}$/'],
             'description' => ['nullable', 'string', 'max:500', 'regex:/.{0,500}/'],
