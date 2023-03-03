aac50edad9ed5f189594f6219f42f9d9042b619c
Ivan Tiutiunnyk <ivan.tiutiunnyk@abz.agency>
2023-03-03 08:41:09 +0200

    feat (paypal): start implementing paypal payments.

--- app/Actions/Payment/NewPayment.php
+++ app/Actions/Payment/NewPayment.php
@@ -7,6 +7,7 @@ namespace App\Actions\Payment;
 use App\Http\Requests\StorePaymentRequest;
 use App\Models\{Order, Payment};
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Str;
 
 class NewPayment
 {
@@ -23,26 +24,50 @@ class NewPayment
         $payment->user()->associate(auth('api')->user());
         $payment->order()->associate($request->validated('order_id'));
 
-        $stripe = new \Stripe\StripeClient(config('services.stripe.api_secret'));
+        if ($request->validated('method') === 'stripe') {
+            $stripe = new \Stripe\StripeClient(config('services.stripe.api_secret'));
 
-        $paymentIntent = $stripe->paymentIntents->create([
-            //            'payment_method_types' => ['card'],
-            'amount' => $payment->amount * 100,
-            'currency' => 'usd',
-            'automatic_payment_methods' => [
-                'enabled' => true,
+            $paymentIntent = $stripe->paymentIntents->create([
+                //            'payment_method_types' => ['card'],
+                'amount' => $payment->amount * 100,
+                'currency' => 'usd',
+                'automatic_payment_methods' => [
+                    'enabled' => true,
+                ],
+            ]);
+
+            $payment->method_id = $paymentIntent->id;
+            $payment->client_secret = $paymentIntent->client_secret;
+
+            $payment->save();
+
+            return response()->json([
+                'type_payment' => 'stripe',
+                'payment_id' => $paymentIntent->id,
+                'client_secret' => $paymentIntent->client_secret,
+                'payment_method' => $paymentIntent->payment_method,
+            ]);
+        }
+        $provider = \PayPal::setProvider();
+        $provider->setApiCredentials(config('paypal'));
+        $token = $provider->getAccessToken();
+        $provider->setRequestHeader('Authorization', 'Bearer ' . $token['access_token']);
+        $provider->setRequestHeader('PayPal-Request-Id', Str::uuid()->toString());
+        $order = $provider->createOrder([
+            'intent' => 'CAPTURE',
+            'purchase_units' => [
+                [
+                    'amount' => [
+                        'currency_code' => 'USD',
+                        'value' => $payment->amount,
+                    ],
+                ],
             ],
         ]);
-
-        $payment->method_id = $paymentIntent->id;
-        $payment->client_secret = $paymentIntent->client_secret;
+        $payment->method_id = $order['id'];
+        $payment->status = $order['status'];
         $payment->save();
 
-        return response()->json([
-            'type_payment' => 'stripe',
-            'payment_id' => $paymentIntent->id,
-            'client_secret' => $paymentIntent->client_secret,
-            'payment_method' => $paymentIntent->payment_method,
-        ]);
+        return response()->json($order);
     }
 }
