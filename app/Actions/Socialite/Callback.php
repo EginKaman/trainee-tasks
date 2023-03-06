5d69a71ff15db64904ee802f5997fff016b5f994
Ivan Tiutiunnyk <ivan.tiutiunnyk@abz.agency>
2023-03-06 17:29:02 +0200

    fix (socialite): fixed localization and exception message.

--- app/Actions/Socialite/Callback.php
+++ app/Actions/Socialite/Callback.php
@@ -18,8 +18,8 @@ class Callback
             $socialiteUser = Socialite::driver($driver)->stateless()->user();
         } catch (ClientException $exception) {
             return response()->json([
-                'message' => __('Bad request'),
-            ], 400);
+                'message' => json_decode((string) $exception->getResponse()->getBody(), false)->message,
+            ], $exception->getCode());
         }
 
         return app(CheckUser::class)->checkOrCreate($socialiteUser, $driver);
