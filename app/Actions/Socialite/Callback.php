94d82d00593e5b5772f282620b0b5a7e8972d33a
Ivan Tiutiunnyk <ivan.tiutiunnyk@abz.agency>
2023-03-09 11:09:58 +0200

    refactor (socialite): refactored actions and controllers.

--- app/Actions/Socialite/Callback.php
+++ app/Actions/Socialite/Callback.php
@@ -4,26 +4,52 @@ declare(strict_types=1);
 
 namespace App\Actions\Socialite;
 
-use App\Http\Resources\UserResource;
-use GuzzleHttp\Exception\ClientException;
-use Illuminate\Http\JsonResponse;
-use Laravel\Socialite\Facades\Socialite;
+use App\Models\{User, User as UserModel, UserProvider};
+use Laravel\Socialite\Two\User as SocialiteUser;
 
 class Callback
 {
-    public function callback(string $driver): UserResource|JsonResponse
-    {
-        try {
-            /** @phpstan-ignore-next-line */
-            $socialiteUser = Socialite::driver($driver)->stateless()->user();
-        } catch (ClientException $exception) {
-            $message = json_decode((string) $exception->getResponse()->getBody(), false);
+    public function __construct(
+        private UpdateProvider $provider,
+        private CreateUser $createUser
+    ) {
+    }
 
-            return response()->json([
-                'message' => $message->error->message ?? $message->error_description,
-            ], $exception->getCode());
+    public function callback(SocialiteUser $socialiteUser, string $driver): User
+    {
+        $userProvider = UserProvider::query()->with('user')
+            ->where('driver', $driver)
+            ->where('driver_id', $socialiteUser->getId())
+            ->firstOrNew([
+                'driver' => $driver,
+                'driver_id' => $socialiteUser->getId(),
+                'token' => $socialiteUser->token,
+                'refresh_token' => $socialiteUser->refreshToken,
+            ]);
+
+        //if user is attached to provider
+        if ($userProvider->exists) {
+            $userProvider = $this->provider->update($userProvider, $socialiteUser);
+
+            return $userProvider->user;
         }
 
-        return app(CheckUser::class)->checkOrCreate($socialiteUser, $driver);
+        $user = UserModel::query()->where('email', $socialiteUser->getEmail())->firstOrNew([
+            'email' => $socialiteUser->getEmail(),
+            'name' => $socialiteUser->getName(),
+        ]);
+
+        //if user exists but didn't have provider
+        if ($user->exists) {
+            $this->provider->update($userProvider, $socialiteUser, $user);
+
+            return $user;
+        }
+
+        $user = $this->createUser->create($user, $socialiteUser);
+
+        $this->provider->update($userProvider, $socialiteUser, $user);
+
+        return $user;
     }
 }
--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Auth;
 
-use App\Actions\Auth\Login;
+use App\Actions\Auth\{Login, LoginLink};
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\{LoginRequest, VerifyRequest};
 use App\Http\Resources\UserResource;
@@ -12,14 +12,31 @@ use Illuminate\Http\JsonResponse;
 
 class LoginController extends Controller
 {
-    public function login(LoginRequest $request, Login $login): JsonResponse
+    public function login(LoginRequest $request, LoginLink $loginLink): JsonResponse
     {
-        return $login->link($request->validated('email'));
+        $loginLink->create($request->validated('email'));
+
+        return response()->json([
+            'status' => __('Success'),
+            'message' => __('A message with an authorization link was successfully sent to your email'),
+        ]);
     }
 
     public function verify(VerifyRequest $request, Login $login): JsonResponse|UserResource
     {
-        return $login->login($request->validated('token'));
+        if ($loginToken = $login->login($request->validated('token'))) {
+            return (new UserResource($loginToken->user))->additional([
+                /** @phpstan-ignore-next-line */
+                'access_token' => auth('api')->login($loginToken->user),
+                'token_type' => 'bearer',
+                /** @phpstan-ignore-next-line */
+                'expires_in' => auth('api')->factory()->getTTL() * 60,
+            ]);
+        }
+
+        return response()->json([
+            'message' => __('Unauthorized'),
+        ], 401);
     }
 
     public function logout(): JsonResponse
