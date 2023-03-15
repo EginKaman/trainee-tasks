8dcfeafde8033bf0758dd253150fc062bf8585eb
Ivan Tiutiunnyk <ivan.tiutiunnyk@abz.agency>
2023-03-15 08:56:50 +0200

    feat (websockets): updated server

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,33 +1,51 @@
 import {createServer} from "http";
+import path from 'path';
 import {Server} from "socket.io"
-import {authorize} from 'socketio-jwt';
-
+import dotenv from 'dotenv';
 import app from "express";
+import Redis from 'ioredis';
 
+dotenv.config({path: path.resolve(process.cwd(), '.env')});
+
+var redis = new Redis(process.env.REDIS_HOST, process.env.REDIS_PORT);
 var server = createServer(app)
 var io = new Server(server);
-require('dotenv').config({path: '../.env'});
 
-server.listen(3000);
-io.use(
-    authorize({
-        secret: process.env.JWT_SECRET,
-        algorithms: [process.env.JWT_ALGO]
+redis.subscribe('test-channel', function (err, count) {
+});
+redis.on('message', function (channel, message) {
+    console.log('Message Recieved: ' + message);
+    message = JSON.parse(message);
+    io.emit(channel + ':' + message.event, message.data);
+});
+
+// io.use(
+//     authorize({
+//         secret: process.env.JWT_SECRET,
+//         algorithms: [process.env.JWT_ALGO]
+//     })
+// )
+io.on('connection', async function (socket) {
+    io.emit('message', {
+        'message': 'You are connected'
     })
-)
-io.on('connection', function (socket) {
     socket.on('users.add', function (message) {
-        io.emit('users.add', message);
+        console.log(message);
+        io.to().emit('users.add', message);
     });
     socket.on('users.update', function (message) {
+        console.log(message);
         io.emit('users.add', message);
     });
     socket.on('users.delete', function (message) {
         io.emit('users.add', message);
     });
-    socket.on('disconnect', function () {
-        socket.broadcast.emit('users.delete', {
+    socket.on('disconnect', () => {
+        console.log('disconnected')
+    });
+});
 
-        })
-    })
+server.listen(process.env.WEBSOCKET_PORT, function () {
+    console.log(`Listening on port ${process.env.WEBSOCKET_PORT}`);
+    console.log(`http://localhost:${process.env.WEBSOCKET_PORT}`);
 });
