4726e7e037a79bae73c8270411060f7427dc0e3b
Ivan Tiutiunnyk <ivan.tiutiunnyk@abz.agency>
2023-08-23 14:19:29 +0300

    feat: deploy to aws

--- deploy.sh
+++ deploy.sh
@@ -75,7 +75,7 @@ print_row() {
 # Ssh functions
 ##################################################################################################
 execute_via_ssh() {
-    ssh -o ProxyCommand="ssh -i ~/.ssh/id_rsa $REMOTE_USER@$SERVER_BASTION_IP nc -w 300 %h %p" "$REMOTE_USER@$SERVER_IP" "$1"
+    ssh -i "~/.ssh/id_rsa" "$REMOTE_USER@$SERVER_IP" -J ""$REMOTE_USER@$SERVER_BASTION_IP"" "$1"
 }
 
 ##################################################################################################
