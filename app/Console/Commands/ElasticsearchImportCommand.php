20df26f9d27b50dd042554025f055dc88469bf90
Ivan Tiutiunnyk <ivan.tiutiunnyk@abz.agency>
2023-09-18 11:02:41 +0300

    feat: optimize serials and movies import

--- app/Console/Commands/ElasticsearchImportCommand.php
+++ app/Console/Commands/ElasticsearchImportCommand.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Console\Commands;
 
 use App\Enum\MediaEnum;
+use App\Models\{Movie, Person, Serial};
 use App\Services\{ElasticsearchService, MovieDbService};
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Storage;
@@ -25,28 +26,32 @@ class ElasticsearchImportCommand extends Command
 
     public function handle(): void
     {
-        foreach (MediaEnum::cases() as $case) {
-            $plural = Str::plural($case->value);
-            $this->client->deleteIndex($plural);
-            $this->client->createIndex($plural);
-
-            $this->withProgressBar($this->json($plural . '.json'), function ($movie) use ($case, $plural): void {
-                $movie = $this->movieDbClient->details($case->value, $movie['id']);
-                if ($case === MediaEnum::Movie && empty($movie['release_date'])) {
-                    $movie['release_date'] = null;
-                }
-                if ($case === MediaEnum::Tv && empty($movie['first_air_date'])) {
-                    $movie['first_air_date'] = null;
-                }
-                if ($case === MediaEnum::Tv && empty($movie['last_air_date'])) {
-                    $movie['last_air_date'] = null;
-                }
-                $this->client->index([
-                    'index' => $plural,
-                    'body' => $movie,
-                ]);
-            });
-        }
+        $this->client->deleteIndex('movies_1693929153');
+        $this->client->deleteIndex('people_1693929342');
+        $this->client->deleteIndex('serials');
+        $this->client->deleteIndex('media');
+//        foreach (MediaEnum::cases() as $case) {
+//            $plural = Str::plural($case->value);
+//            $this->client->deleteIndex($plural);
+//            $this->client->createIndex($plural);
+//
+//            $this->withProgressBar($this->json($plural . '.json'), function ($movie) use ($case, $plural): void {
+//                $movie = $this->movieDbClient->details($case->value, $movie['id']);
+//                if ($case === MediaEnum::Movie && empty($movie['release_date'])) {
+//                    $movie['release_date'] = null;
+//                }
+//                if ($case === MediaEnum::Tv && empty($movie['first_air_date'])) {
+//                    $movie['first_air_date'] = null;
+//                }
+//                if ($case === MediaEnum::Tv && empty($movie['last_air_date'])) {
+//                    $movie['last_air_date'] = null;
+//                }
+//                $this->client->index([
+//                    'index' => $plural,
+//                    'body' => $movie,
+//                ]);
+//            });
+//        }
     }
 
     private function json(string $filename): array
--- database/seeders/MovieSeeder.php
+++ database/seeders/MovieSeeder.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace Database\Seeders;
 
 use App\Enum\MediaEnum;
+use App\Jobs\ImportMovieJob;
 use App\Models\{Country, Movie};
-use App\Services\MovieDbService;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\{Arr, Str};
@@ -18,15 +18,13 @@ class MovieSeeder extends Seeder
     {
         $type = MediaEnum::Movie;
 
-        $movieDbClient = new MovieDbService();
-
         try {
-            $movieDataDump = json_decode(
+            $movieDataDump = array_chunk(json_decode(
                 Storage::get(Str::plural($type->value) . '.json'),
                 true,
                 512,
                 JSON_THROW_ON_ERROR
-            );
+            ), 512);
         } catch (JsonException $e) {
             $this->command->error($e->getMessage());
 
@@ -35,47 +33,8 @@ class MovieSeeder extends Seeder
 
         $this->command->withProgressBar(
             $movieDataDump,
-            function ($movieDetails) use ($movieDbClient, $type): void {
-                $movieDetails = $movieDbClient->details($type->value, $movieDetails['id']);
-                if (empty($movieDetails['release_date'])) {
-                    $movieDetails['release_date'] = null;
-                }
-                if (empty($movieDetails['homepage'])) {
-                    $movieDetails['homepage'] = null;
-                }
-                if (empty($movieDetails['overview'])) {
-                    $movieDetails['overview'] = null;
-                }
-                if (empty($movieDetails['tagline'])) {
-                    $movieDetails['tagline'] = null;
-                }
-
-                $movie = Movie::updateOrCreate([
-                    'id' => Arr::get($movieDetails, 'id'),
-                ], Arr::only($movieDetails, [
-                    'adult',
-                    'budget',
-                    'homepage',
-                    'imdb_id',
-                    'original_language',
-                    'original_title',
-                    'overview',
-                    'popularity',
-                    'release_date',
-                    'revenue',
-                    'runtime',
-                    'status',
-                    'tagline',
-                    'title',
-                    'vote_average',
-                    'vote_count',
-                ]));
-
-                $movie->genres()->sync(Arr::map(Arr::get($movieDetails, 'genres'), fn ($genre) => $genre['id']));
-                $movie->countries()->sync(Country::whereIn(
-                    'iso_3166_1',
-                    Arr::map(Arr::get($movieDetails, 'production_countries'), fn ($country) => $country['iso_3166_1'])
-                )->get());
+            function ($movieIds): void {
+                ImportMovieJob::dispatch($movieIds)->onQueue('import');
             }
         );
     }
